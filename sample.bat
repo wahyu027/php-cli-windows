c73a37ab7ea5991c85175ea970e49d9e70deabfc
Wahyu Hidayat <wahyu.hidayat3@gmail.com>
2021-01-01 13:07:58 +0700

    Add sample

---
+++ sample.bat
@@ -0,0 +1,20 @@
+@echo off
+
+echo:
+echo:
+echo    _                ___       _.--.
+echo    \`.^|\..----...-'`   `-._.-'_.-'`
+echo    /  ' `         ,       __.--'
+echo   )/' _/     \   `-_,   /
+echo    `-'" `"\_  ,_.-;_.-\_ ',     
+echo        _.-'_./   {_.'   ; /
+echo       {_.-``-'         {_/                           
+echo:
+echo:
+echo MY SIMPE APP
+echo:
+echo:
+
+php\php.exe sample.php
+
+pause
---
+++ sample.php
@@ -0,0 +1,4 @@
+<?php
+
+echo "Hello\n\n";
+echo "This is my simple PHP CLI app\n\n\n";
